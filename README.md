3ab51b92e57caf67d015fb2511bafbd8f7d49d59
IlyaK <ilyxakotsur@gmail.com>
2022-06-28 20:58:43 +0300

    add linkedList and binary tree

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DataStructure
+
 Basic data structures on JS
+
+[Статья про алгоритмы и структуры данных] (https://proglib.io/p/rasprostranennye-algoritmy-i-struktury-dannyh-v-javascript-derevya-2021-10-18)
+[Бинарное дерево поиска] (https://youtu.be/fnqUD4FTE5Q)
---
+++ linked list/linkedList.js
@@ -0,0 +1,136 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = newNode(value);
+    }
+    this.length++;
+  }
+
+  insert(position, value) {
+    if (position < 0 || position > this.length) {
+      return false;
+    }
+
+    let node = new Node(value);
+
+    if (position === 0) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      let current = this.head;
+      let prev = null;
+
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = node;
+      node.next = current;
+    }
+
+    this.length++;
+  }
+
+  get(position) {
+    if (position < 0 || position >= this.length) {
+      return;
+    }
+
+    let current = this.head;
+    let index = 0;
+
+    while (index < position) {
+      current = current.next;
+      index++;
+    }
+
+    return current.value;
+  }
+
+  removeAt(position) {
+    if (position < 0 || position >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+
+    if (position === 0) {
+      this.head = current.next;
+    } else {
+      let prev = null;
+      let index = 0;
+
+      while (index < position) {
+        prev = current;
+        current = current.next;
+        index++;
+      }
+
+      prev.next = current.next;
+    }
+
+    this.length--;
+    return current.value;
+  }
+
+  remove(element) {
+    this.removeAt(this.indexOf(element));
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.value === element) {
+        return index;
+      }
+
+      current = current.next;
+      index++;
+    }
+
+    return -1;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  print() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
